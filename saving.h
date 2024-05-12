84596a202854071e797bb689694dcdf7ed968db8
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-12 18:55:46 +0100

    Update saving.h

--- saving.h
+++ saving.h
@@ -9,4 +9,5 @@ int trouver_bias(char ch[],char *filename);
 int trouver_weight(char ch[],char *filename);
 void afficher(char *filename);
 void min(char *filename, double *minimum, char *ligne);
+void remp_excel (char *filename ,char *filename1);
 #endif
